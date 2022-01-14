d4dbc9dd7874d7a89faa7346a8c46aa00191c643
Sammatha@99 <ngonhatrang99@gmail.com>
2022-01-14 15:44:30 +0700

    deploy heroku

--- src/config/config.js
+++ src/config/config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
   },
   jwt: {
-    secret: envVars.JWT_SECRET,
+    secret: `wetextappkey321`,
     accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
     resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 // const http = require('http');
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
-const { userService, chatroomService } = require('./services');
+// const { userService, chatroomService } = require('./services');
 
 let server;
-let io;
-const usersLogin = [];
+// let io;
+// const usersLogin = [];
 
-const usersLoginPush = (newUser) => {
-  const findIndex = usersLogin.findIndex((user) => user.userId === newUser.userId);
-  if (findIndex === -1) {
-    usersLogin.push(newUser);
-  }
-};
+// const usersLoginPush = (newUser) => {
+//   const findIndex = usersLogin.findIndex((user) => user.userId === newUser.userId);
+//   if (findIndex === -1) {
+//     usersLogin.push(newUser);
+//   }
+// };
 
-const usersLoginRemove = (removeUser) => {
-  const findIndex = usersLogin.findIndex((user) => user.userId === removeUser.userId);
-  if (findIndex !== -1) {
-    usersLogin.splice(findIndex, 1);
-  }
-};
+// const usersLoginRemove = (removeUser) => {
+//   const findIndex = usersLogin.findIndex((user) => user.userId === removeUser.userId);
+//   if (findIndex !== -1) {
+//     usersLogin.splice(findIndex, 1);
+//   }
+// };
 
 const PORT = process.env.PORT || 3000;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
@@ -30,74 +30,74 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   server = app.listen(PORT, () => {
     logger.info(`Listening to port ${PORT}`);
   });
-  io = socketio(server, {
-    cors: {
-      origin: ['http://localhost:3006'],
-    },
-  });
-  io.on('connect', (socket) => {
-    socket.on('login', async (userId) => {
-      usersLoginPush({ userId, socketId: socket.id });
-      socket.broadcast.emit('login', userId);
-      const chatroomsId = await chatroomService.getAllChatroomsIdByUserId(userId);
-      const chatroomsIdString = Array.from(chatroomsId, (id) => id.toString());
-      socket.join(chatroomsIdString);
-    });
+  // io = socketio(server, {
+  //   cors: {
+  //     origin: ['http://localhost:3006'],
+  //   },
+  // });
+  // io.on('connect', (socket) => {
+  //   socket.on('login', async (userId) => {
+  //     usersLoginPush({ userId, socketId: socket.id });
+  //     socket.broadcast.emit('login', userId);
+  //     const chatroomsId = await chatroomService.getAllChatroomsIdByUserId(userId);
+  //     const chatroomsIdString = Array.from(chatroomsId, (id) => id.toString());
+  //     socket.join(chatroomsIdString);
+  //   });
 
-    socket.on('logout', async (userId) => {
-      userService.updateUserById(userId, { status: false });
-      usersLoginRemove({ userId, socketId: socket.id });
-      socket.broadcast.emit('logout', userId);
-      const { rooms } = socket;
-      rooms.forEach((room) => socket.leave(room));
-    });
+  //   socket.on('logout', async (userId) => {
+  //     userService.updateUserById(userId, { status: false });
+  //     usersLoginRemove({ userId, socketId: socket.id });
+  //     socket.broadcast.emit('logout', userId);
+  //     const { rooms } = socket;
+  //     rooms.forEach((room) => socket.leave(room));
+  //   });
 
-    socket.on('create-chatroom', (senderId, chatroomId, membersId) => {
-      usersLogin.forEach((user) => {
-        if (membersId.includes(user.userId)) {
-          io.to(user.socketId).emit('new-chatroom', senderId, chatroomId);
-        }
-      });
-    });
+  //   socket.on('create-chatroom', (senderId, chatroomId, membersId) => {
+  //     usersLogin.forEach((user) => {
+  //       if (membersId.includes(user.userId)) {
+  //         io.to(user.socketId).emit('new-chatroom', senderId, chatroomId);
+  //       }
+  //     });
+  //   });
 
-    socket.on('join-room', (chatroomId) => {
-      socket.join(chatroomId);
-    });
+  //   socket.on('join-room', (chatroomId) => {
+  //     socket.join(chatroomId);
+  //   });
 
-    socket.on('send-message', (message, sender) => {
-      socket.broadcast.to(message.chatroomId).emit('receive-message', message, sender);
-      socket.broadcast.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
-    });
+  //   socket.on('send-message', (message, sender) => {
+  //     socket.broadcast.to(message.chatroomId).emit('receive-message', message, sender);
+  //     socket.broadcast.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
+  //   });
 
-    socket.on('send-add-member', (chatroomId, lastMessageId, time, newMembersId) => {
-      // gửi cho Global
-      io.to(chatroomId).emit('receive-add-member', chatroomId);
+  //   socket.on('send-add-member', (chatroomId, lastMessageId, time, newMembersId) => {
+  //     // gửi cho Global
+  //     io.to(chatroomId).emit('receive-add-member', chatroomId);
 
-      // gửi cho chat body
-      io.to(chatroomId).emit(`receive-add-member-${chatroomId}`, chatroomId, lastMessageId, time, newMembersId);
+  //     // gửi cho chat body
+  //     io.to(chatroomId).emit(`receive-add-member-${chatroomId}`, chatroomId, lastMessageId, time, newMembersId);
 
-      // mời các thành viên mới vào
-      usersLogin.forEach((user) => {
-        if (newMembersId.includes(user.userId)) {
-          io.to(user.socketId).emit('new-chatroom', '', chatroomId);
-        }
-      });
-    });
+  //     // mời các thành viên mới vào
+  //     usersLogin.forEach((user) => {
+  //       if (newMembersId.includes(user.userId)) {
+  //         io.to(user.socketId).emit('new-chatroom', '', chatroomId);
+  //       }
+  //     });
+  //   });
 
-    socket.on('send-remove-member', (message, sender) => {
-      socket.broadcast.to(message.chatroomId).emit('receive-remove-member', message, sender);
-      socket.broadcast.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
-    });
+  //   socket.on('send-remove-member', (message, sender) => {
+  //     socket.broadcast.to(message.chatroomId).emit('receive-remove-member', message, sender);
+  //     socket.broadcast.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
+  //   });
 
-    socket.on('send-chatroom-name', (newName, message, sender) => {
-      socket.broadcast.to(message.chatroomId).emit('receive-chatroom-name', newName, message);
-      io.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
-    });
+  //   socket.on('send-chatroom-name', (newName, message, sender) => {
+  //     socket.broadcast.to(message.chatroomId).emit('receive-chatroom-name', newName, message);
+  //     io.to(message.chatroomId).emit(`receive-message-${message.chatroomId}`, message, sender);
+  //   });
 
-    socket.on('send-seen-message', (chatroomId, seenHistory) => {
-      socket.to(chatroomId).emit('receive-seen-message', chatroomId, seenHistory);
-    });
-  });
+  //   socket.on('send-seen-message', (chatroomId, seenHistory) => {
+  //     socket.to(chatroomId).emit('receive-seen-message', chatroomId, seenHistory);
+  //   });
+  // });
   // server.listen(process.env.PORT || 3000, () => {
   //   logger.info(`Listening to port ${process.env.PORT || 3000}`);
   // });
