5368e41296142e6eeeaeb97e4c05998d64e9ba67
Sammatha@99 <ngonhatrang99@gmail.com>
2022-01-14 15:50:11 +0700

    deploy heroku

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,13 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
+    MONGODB_URL: Joi.string()
+      .required()
+      .default(
+        'mongodb+srv://trangAdmin:trangAdmin@cluster0.ckkl7.mongodb.net/we-text-database-testing?retryWrites=true&w=majority'
+      )
+      .description('Mongo DB url'),
+    JWT_SECRET: Joi.string().default(`wetextappkey321`).required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
@@ -18,11 +23,13 @@ const envVarsSchema = Joi.object()
     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
       .default(5)
       .description('minutes after which verify email token expires'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    SMTP_HOST: Joi.string().default(`smtp.gmail.com`).description('server that will send the emails'),
+    SMTP_PORT: Joi.number().default(465).description('port to connect to the email server'),
+    SMTP_USERNAME: Joi.string().default('itcoupletesting@gmail.com').description('username for email server'),
+    SMTP_PASSWORD: Joi.string().default(`ITCOUPLETESTING9998`).description('password for email server'),
+    EMAIL_FROM: Joi.string()
+      .default('itcoupletesting@gmail.com')
+      .description('the from field in the emails sent by the app'),
   })
   .unknown();
 
